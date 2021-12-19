d4b821af11893c20c45b809c34f9f98d9d105fd2
andreaspencer <andreaaddawn@gmail.com>
2021-12-19 14:33:34 -0800

    changes

--- public/js/idb.js
+++ public/js/idb.js
@@ -6,11 +6,11 @@ window.msIndexedDB ||
 window.shimIndexedDB;
 
 let db;
-const request = indexedDB.open('budget-tracker', 1);
+const request = indexedDB.open("budget-tracker", 1);
 
 request.onupgradeneeded = function(event) {
     const db = event.target.result;
-    db.createObjectStore('new_transaction', { keyPath: "id", autoIncrement: true });
+    db.createObjectStore("new_transaction", { keyPath: "id", autoIncrement: true });
 };
 
 request.onerror = function(event) {
@@ -26,30 +26,30 @@ request.onsuccess = function(event) {
 };
 
 function saveRecord(record) {
-    const transaction = db.transaction('new_transaction', 'readwrite');
-    const budgetObjectStore = transaction.objectStore('new_transaction');
+    const transaction = db.transaction("new_transaction", "readwrite");
+    const budgetObjectStore = transaction.objectStore("new_transaction");
     budgetObjectStore.add(record);
 };
 
 function uploadTransaction() {
-    const transaction = db.transaction('new_transaction', 'readwrite');
-    const budgetObjectStore = transaction.objectStore('new_transaction');
+    const transaction = db.transaction("new_transaction", "readwrite");
+    const budgetObjectStore = transaction.objectStore("new_transaction");
     const getAll = budgetObjectStore.getAll();
 
     getAll.onsuccess = function() {
         if (getAll.result.length > 0) {
             fetch('/api/transaction/bulk', {
-                method: 'POST',
+                method: "POST",
                 body: JSON.stringify(getAll.result),
                 headers: {
-                    Accept: 'application/json, text/plain, */*',
-                    'Content-Type': 'application.json'
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application.json"
                 }
             })
             .then((response) => response.json())
             .then(() => {
-                const transaction = db.transaction('new_transaction', 'readwrite');
-                const budgetObjectStore = transaction.objectStore('new_transaction');
+                const transaction = db.transaction("new_transaction", "readwrite");
+                const budgetObjectStore = transaction.objectStore("new_transaction");
                 budgetObjectStore.clear();
             })
             .catch(err => {
@@ -59,4 +59,4 @@ function uploadTransaction() {
     }
 }
 
-window.addEventListener('online', uploadTransaction);
+window.addEventListener("online", uploadTransaction);
