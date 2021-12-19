e99b6f4cd4caa02ede645017512dd7c6a8843725
andreaspencer <andreaaddawn@gmail.com>
2021-12-19 15:39:45 -0800

    debugging

--- public/js/idb.js
+++ public/js/idb.js
@@ -6,7 +6,7 @@ window.msIndexedDB ||
 window.shimIndexedDB;
 
 let db;
-const request = indexedDB.open("budget-tracker", 1);
+const request = indexedDB.open("budget_tracker", 1);
 
 request.onupgradeneeded = function(event) {
     const db = event.target.result;
