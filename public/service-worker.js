93dc8d973a414ae63caed15b607a8585daca17a1
andreaspencer <andreaaddawn@gmail.com>
2022-01-21 19:53:32 -0800

    grammar correction

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,11 @@
 const APP_PREFIX = 'BudgetApp-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
+const DATA_CACHE_NAME = "data-cache-" + VERSION;
 
 const FILES_TO_CACHE = [
     './index.html',
-    './css/style.css',
+    './css/styles.css',
     './js/index.js',
     './js/idb.js',
     './manifest.json',
@@ -27,7 +28,45 @@ self.addEventListener('install', function (e) {
     )
 });
 
-self.addEventListener('activate', function (e) {
+self.addEventListener("fetch", function(event) {
+    // cache all get requests to /api routes
+    if (event.request.url.includes("/api/")) {
+      event.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(event.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+  
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(event.request);
+            });
+        }).catch(err => console.log(err))
+      );
+  
+      return;
+    }
+  
+    event.respondWith(
+      fetch(event.request).catch(function() {
+        return caches.match(event.request).then(function(response) {
+          if (response) {
+            return response;
+          } else if (event.request.headers.get("accept").includes("text/html")) {
+            // return the cached home page for all requests for html pages
+            return caches.match("/");
+          }
+        });
+      })
+    );
+  });
+
+/*self.addEventListener('activate', function (e) {
     e.waitUntil(
         caches.keys().then(function (keyList) {
             let cacheKeeplist = keyList.filter(function (key) {
@@ -43,9 +82,9 @@ self.addEventListener('activate', function (e) {
             }));
         })
     )
-});
+});*/
 
-self.addEventListener('fetch', function (e) {
+/*self.addEventListener('fetch', function (e) {
     console.log('fetch request : ' + e.request.url);
     e.respondWith(
         caches.match(e.request).then(function (request) {
@@ -58,4 +97,4 @@ self.addEventListener('fetch', function (e) {
             }
         })
     )
-});
+});*/
